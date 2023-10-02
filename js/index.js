3e7ac0b1b060a47c33e14078c20478016c37b61e
Kowndinya2000 <kb1204@scarletmail.rutgers.edu>
2023-10-02 13:01:20 -0400

    update: data, images

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+    $('.publication-mousecell').mouseover(function () {
+        $(this).find('video').css('display', 'inline-block');
+        $(this).find('img').css('display', 'none');
+    });
+    $('.publication-mousecell').mouseout(function () {
+        $(this).find('video').css('display', 'none');
+        $(this).find('img').css('display', 'inline-block');
+    });
+    var ids = ["ovsg", "egen", "repoquester", "gitq", "annotator"]
+    for (var i = 0; i < ids.length; i++) {
+        var id = ids[i];
+        console.log("project_page_" + id)
+        console.log(document.getElementById("project_page_" + id).href);
+        if (document.getElementById("project_page_" + id).href == "" || document.getElementById("project_page_" + id).href == "https://") {
+            document.getElementById("project_page_" + id).style.display = "none";
+            document.getElementById("project_page_" + id).style.visibility = "hidden";
+        }
+        console.log(document.getElementById("arxiv_page_" + id).href);
+        if (document.getElementById("arxiv_page_" + id).href == "https://arxiv.org/pdf/.pdf") {
+            document.getElementById("arxiv_page_" + id).style.display = "none";
+            document.getElementById("arxiv_page_" + id).style.visibility = "hidden";
+        }
+
+    }
+
+})
+
